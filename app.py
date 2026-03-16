e7fccfdf81afc56f9810e94a42c0b7cd9a7ca067
Domini Oliveira Nascimento <dominioliveiranascimento@gmail.com>
2026-03-16 20:09:08 -0300

    att port
    Comment out the PORT variable and the main execution block.

--- app.py
+++ app.py
@@ -280,8 +280,8 @@ def logout():
     return redirect("/login")
 
 
-PORT = int(os.environ.get("PORT", 5000))
+#PORT = int(os.environ.get("PORT", 5000))
     
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=PORT)
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port=PORT)
     
